9296aa2ae04442abeab0b5866bfae7ee16f32d4a
Esther-Wagatwe <estherwagatwe@gmail.com>
2023-09-28 15:55:02 +0300

    1-print_binary.cr

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to print in binary
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int currentbit;
+
+	for (i = 63; i >= 0; i--)
+	{
+		currentbit = n >> i;/* Right-shift 'n' by 'i' bits */
+
+		if (currentbit & 1)
+		{
+			_putchar('1'); /* Print '1' for a set bit */
+			count++;
+		}
+		else if (count)
+		{
+			_putchar('0'); /* Print '0' for trailing zeros */
+		}
+	}
+	if (!count)
+	{
+		_putchar('0'); /* If there are no significant bits, print '0' */
+	}
+}
