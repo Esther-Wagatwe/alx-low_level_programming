ddb3d53753370d0f3d6416b8f3f8c8c1652bd884
Esther-Wagatwe <estherwagatwe@gmail.com>
2023-10-26 12:36:46 +0300

    Betty corrections

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,7 +3,7 @@
 
 /**
  * get_bit -  function that returns the value of a bit at a given index.
- @n: the binary number to check for the bit value
+ * @n: the binary number to check for the bit value
  * @index: is the index, starting from 0 of the bit you want to get
  * Return: the value of the bit at index index or -1 if an error occured
  */
